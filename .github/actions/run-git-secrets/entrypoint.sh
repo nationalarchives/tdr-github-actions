ec25ccb50e9319d413a079cfb83a770714017c4b
TomJKing <tom.king@nationalarchives.gov.uk>
2022-04-13 10:29:43 +0100

    Set workspace as safe
    Git suggest making this change.
    
    Due to a security alert: https://github.blog/2022-04-12-git-security-vulnerability-announced/
    
    See this stackoverflow thread: https://stackoverflow.com/questions/71849415/cannot-add-parent-directory-to-safe-directory-on-git

--- .github/actions/run-git-secrets/entrypoint.sh
+++ .github/actions/run-git-secrets/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
+git config --global --add safe.directory '/github/workspace' && \
 git-secrets --register-aws --global && \
 git-secrets --add --global '([^0-9])*[0-9]{12}([^0-9])*'
 git-secrets --add --global --allowed '1234'
