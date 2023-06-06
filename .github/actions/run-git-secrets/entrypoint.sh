fa0c3e4dff09afb0b55224ad46b4494d5ead400c
Sam Palmer <sam.palmer@nationalarchives.gov.uk>
2023-06-06 17:22:16 +0100

    Add AWS AMI account to allowed secrets
    This account belongs to AWS and isn't secret. It's used when we're matching a given AMI.

--- .github/actions/run-git-secrets/entrypoint.sh
+++ .github/actions/run-git-secrets/entrypoint.sh
@@ -3,4 +3,5 @@ git config --global --add safe.directory '/github/workspace' && \
 git-secrets --register-aws --global && \
 git-secrets --add --global '([^0-9])*[0-9]{12}([^0-9])*'
 git-secrets --add --global --allowed '1234'
+git-secrets --add --global --allowed '137112412989'
 git-secrets --scan
